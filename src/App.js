8718d27c47cd1ee7b829ad1285d46f5a9c7ef457
Paulo Henrique <paulotux.mendes@gmail.com>
2020-10-18 23:08:54 -0300

    [UPDATE] Criando primeiro projeto com React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import Car from './Car'
 import './App.css';
 
 function App() {
@@ -8,16 +9,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Criando a primeira SPA React App1
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Car colorido='blue'/>
       </header>
     </div>
   );
